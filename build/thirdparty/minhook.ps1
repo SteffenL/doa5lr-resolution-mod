a86cc9f2507fa6421d59a3d94e26cb42d734e4d3
Steffen André Langnes <steffenl.sw@gmail.com>
2015-04-02 05:47:13 +0200

    Use forked repo of minhook

--- build/thirdparty/minhook.ps1
+++ build/thirdparty/minhook.ps1
@@ -9,13 +9,6 @@ $solutionFilePath = "MinHookVC12.sln"
 # Change the build config
 
 $projectFilePaths = "libMinHook.vcxproj"
-$projectFilePaths | %{
-    $projectFilePath = $_
-    $projectContent = Get-Content -Path $projectFilePath
-    $projectContent = $projectContent -replace '(?<=<RuntimeLibrary>)MultiThreadedDebug(?=</RuntimeLibrary>)', "MultiThreadedDebugDLL"
-    #$projectContent = $projectContent -replace '(?<=<RuntimeLibrary>)MultiThreaded(?=</RuntimeLibrary>)', "MultiThreadedDLL"
-    $projectContent | Set-Content -Path $projectFilePath
-}
 
 # Build
 &msbuild $solutionFilePath /t:Rebuild /p:Configuration=Debug
