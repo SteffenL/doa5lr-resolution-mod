ca11216262f8157528993d722779683c59399a4e
Steffen André Langnes <steffenl.sw@gmail.com>
2020-06-28 00:04:34 +0900

    Set DLL version from CMake

--- src/version.hpp
+++ src/version.hpp
@@ -1,9 +1,5 @@
 #pragma once
 
 #define MOD_NAME "DO5LR Resolution Mod"
-// Remember to set this in the *.rc file
-#ifndef MOD_VERSION
-#define MOD_VERSION "dev"
-#endif
 #define MOD_AUTHOR "Steffen André langnes"
 #define MOD_WEBSITE "https://www.steffenl.com"
