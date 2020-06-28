0a005c5ae0acab01d972c69864a6888e72d61a66
Steffen André Langnes <steffenl.sw@gmail.com>
2020-06-28 17:06:11 +0900

    Logger

--- src/log/Logger.hpp
+++ src/log/Logger.hpp
@@ -5,56 +5,18 @@
 
 class Logger {
 public:
-    Logger(LogLevel logLevel) : m_logLevel{logLevel} {}
-    virtual ~Logger() = default;
+    Logger(LogLevel logLevel);
+    virtual ~Logger();
 
     virtual void doLog(const std::string& message) const = 0;
 
-    void log(LogLevel level, const std::string& message) const {
-        if (isLogEnabled(level)) {
-            std::string s;
-            s += "[";
-            s += getLogLevelName(level);
-            s += "] ";
-            s += message;
-            doLog(s);
-        }
-    }
-
-    bool isLogEnabled(LogLevel level) const {
-        int levelInt = static_cast<int>(level);
-        int currentLevelInt = static_cast<int>(m_logLevel);
-        return levelInt <= currentLevelInt;
-    }
-
-    void info(const std::string& message) const {
-        log(LogLevel::Info, message);
-    }
-
-    void error(const std::string& message) const {
-        log(LogLevel::Error, message);
-    }
-
-    void debug(const std::string& message) const {
-        log(LogLevel::Debug, message);
-    }
-
-    LogLevel getLogLevel() const {
-        return m_logLevel;
-    }
-
-    std::string getLogLevelName(LogLevel level) const {
-        switch (level) {
-            case LogLevel::Error:
-                return "error";
-            case LogLevel::Info:
-                return "info";
-            case LogLevel::Debug:
-                return "debug";
-        }
-
-        return "";
-    }
+    void log(LogLevel level, const std::string& message) const;
+    bool isLogEnabled(LogLevel level) const;
+    void info(const std::string& message) const;
+    void error(const std::string& message) const;
+    void debug(const std::string& message) const;
+    LogLevel getLogLevel() const;
+    std::string getLogLevelName(LogLevel level) const;
 
 private:
     LogLevel m_logLevel{LogLevel::Silent};
