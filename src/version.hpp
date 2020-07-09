c590abd120c4234bfca387b9ea1833d9e171b7f2
Steffen André Langnes <steffenl.sw@gmail.com>
2020-07-10 01:22:03 +0900

    Fix typo in project name

--- src/version.hpp
+++ src/version.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-#define MOD_NAME "DO5LR Resolution Mod"
+#define MOD_NAME "DOA5LR Resolution Mod"
 #define MOD_AUTHOR "Steffen André langnes"
 #define MOD_WEBSITE "https://www.steffenl.com"
